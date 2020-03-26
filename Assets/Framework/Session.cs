261e5f7e99f1e92171640fba01f2aa0f09f72a4d
mdvulfix <mdvulfix@users.noreply.github.com>
2020-03-27 01:25:47 +0300

    27.03.2020 DB
    27.03.2020 DB

---
+++ Assets/Framework/Session.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace FinanceProgram.Framework
+{
+    [System.Serializable]
+    public class Session : MonoBehaviour
+    {
+
+        [SerializeField] private SystemDataBase dataBase;
+
+
+    }
+}
---
+++ Assets/Framework/Systems/SystemDataBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Configuration;
+using System.Security;
+using System.EnterpriseServices;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+[System.Serializable]
+public class SystemDataBase : MonoBehaviour
+{
+    public string login;
+    public string password;
+
+}
