a6f0168bd36a7ef5361fa06a75ddf15688c66f1f
mdvulfix <mdvulfix@users.noreply.github.com>
2020-03-28 01:15:04 +0300

    28.03.2020 DB Connecton
    28.03.2020 DB Connecton

--- Assets/Framework/Systems/SystemDataBase.cs
+++ Assets/Framework/Systems/SystemDataBase.cs
@@ -1,14 +1,110 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections;
+using System.Data;
 using System.Configuration;
 using System.Security;
 using System.EnterpriseServices;
+using System.IO;
 using Mono.Data.Sqlite;
 using UnityEngine;
+using UnityEngine.UI;
 
-[System.Serializable]
-public class SystemDataBase : MonoBehaviour
+namespace FinanceProgram.Framework
 {
-    public string login;
-    public string password;
+    
+    [System.Serializable]
+    public class SystemDataBase : MonoBehaviour
+    {
+        public string login;
+        public string password;
+        public Text status;
+        
+        public static DataBase dataBase = new DataBase();
 
+        private void Awake()
+        {
+            status.text = "Connection is absent!";
+
+        }
+
+        public void Connect()
+        {
+            string state;
+            dataBase.OnConnect(out state);
+            status.text = state;
+        }
+                
+        
+        public void Request()
+        {
+            var request = "Select * from Users";
+            dataBase.OnCommand(request);
+
+        }
+
+        public void Read(SqliteCommand command)
+        {
+            var request = "Select * from Users";
+            dataBase.OnCommand(request);
+
+        }
+
+
+
+    }
+
+    //TODO: Reclass DataBaseConnection
+    public class DataBase
+    {
+        public SqliteConnection Connection {get; private set;}
+        private string path;
+        
+        private SqliteCommand command;
+        private SqliteDataReader reader;
+        
+        public bool OnConnect(out string status) 
+        {
+            
+            status = "Connection is absent!";
+            
+            try
+            {
+                path = Application.dataPath + "/Source/DataBase/financeprogramdb.bytes";
+                Connection = new SqliteConnection("URI=file:" + path );
+                Connection.Open();
+
+                if(Connection.State == ConnectionState.Open)
+                    status = path.ToString() + "has been connected!";
+
+                
+                return true;
+            }
+            catch(Exception exeption)
+            {
+
+                status = exeption.ToString();
+                return false;
+            }
+        
+        }  
+
+        public bool OnCommand(string request)
+        {
+            try
+            {
+                command = new SqliteCommand(request, Connection);      
+                return true;
+            }
+            catch(Exception exeption)
+            {
+                Debug.Log(exeption.ToString());
+                return false;
+            }
+
+
+        }
+ 
+
+
+    }
 }
